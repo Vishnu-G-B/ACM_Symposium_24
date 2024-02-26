80ece737aa9f97728685af46f78b75a2721c3e3c
Vishnu Bhattacharya <vishnubhattacharya.04@gmail.com>
2024-02-26 15:54:02 +0530

    added daisy, gsap, tailwind, etc.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte";
+    import gsap from "gsap/dist/gsap";
+
+    onMount(() => {
+        const ctx = gsap.context(() => {
+            const tl = gsap.timeline();
+            tl.to(".box", {x:300, y:300, rotation: 2880, duration: 3})
+        })
+    });
+
+</script>
+
+<section class="min-h-screen">
+
+</section>
+
+<style>
+
+</style>
