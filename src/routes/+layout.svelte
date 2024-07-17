c6928103a3536370319c980bb215021c9e3f8628
Vishnu Bhattacharya <vishnubhattacharya.04@gmail.com>
2024-07-17 23:27:57 +0530

    testing some larceny

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,13 @@
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 
-	inject({ mode: dev ? 'development' : 'production' });
+	inject({
+		mode: dev ? 'development' : 'production',
+		beforeSend: (event) => {
+			console.log(event);
+			return event;
+		}
+	});
 	// console.log($page.data.session);
 </script>
 
